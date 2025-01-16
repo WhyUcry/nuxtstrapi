5bff13b5fedc39b95965405d4a49dd8f00fc9cbe
WhyUcry <iemelvan67@gmail.com>
2025-01-16 20:20:10 +0300

    update

--- client/pages/contact.vue
+++ client/pages/contact.vue
@@ -3,7 +3,7 @@
         <h1 class="text-4xl font-extrabold my-4 dark:text-white">Контакты</h1>
         <div class="flex gap-32 items-center mt-16 mb-20">
             <div>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d762.5071661811821!2d39.725774475768695!3d43.59295583265837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f5cba53a28206d%3A0x15959bb73136c25d!2z0JrQvtC80L_RjNGO0YLQtdGA0L3QsNGPINCw0LrQsNC00LXQvNC40Y8gVE9Q!5e1!3m2!1sru!2sru!4v1734019274060!5m2!1sru!2sru" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-lg relative">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d762.5071661811821!2d39.725774475768695!3d43.59295583265837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f5cba53a28206d%3A0x15959bb73136c25d!2z0JrQvtC80L_RjNGO0YLQtdGA0L3QsNGPINCw0LrQsNC00LXQvNC40Y8gVE9Q!5e1!3m2!1sru!2sru!4v1734019274060!5m2!1sru!2sru" width="1600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-lg relative">
                     
                     <div class="absolute">
                         <h3 class="text-2xl my-4">Вы можете связаться с нами:</h3>
